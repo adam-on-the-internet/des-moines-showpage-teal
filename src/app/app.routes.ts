e5fe7a04f93598130df9a4088e314ad2add12c30
Adam Callanan <a.callanan@sbcglobal.net>
2024-10-28 23:12:19 -0500

    start building vanilla content for showpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {UpcomingShowsComponent} from './components/upcoming-shows/upcoming-shows.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'upcoming-shows', component: UpcomingShowsComponent},
+  {path: '', redirectTo: '/upcoming-shows', pathMatch: 'full'},
+];
---
+++ src/app/components/upcoming-shows/upcoming-shows.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-shows',
+  standalone: true,
+  imports: [],
+  templateUrl: './upcoming-shows.component.html',
+  styleUrl: './upcoming-shows.component.css'
+})
+export class UpcomingShowsComponent {
+
+}
